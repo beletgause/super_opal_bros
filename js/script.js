f5da553a1e16492f8926a38b12442cd6929e7323
beletgause <50548162+beletgause@users.noreply.github.com>
2021-06-24 15:13:10 +0300

    remove animate

--- js/script.js
+++ js/script.js
@@ -40,14 +40,7 @@ function go(){
 	setTimeout(function() {window.location.href='game.html';}, 25000);//спустя 25 секунд перенаправляет на страницу игры
 
 }
-jQuery(document).ready(function(){//анимация теней текста
-   $('body').mousemove(function(e){
-     var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
-     var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
-     $('ul').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(227,6,19,.8), '+rYP/8+'px '+rXP/60+'px rgba(255,237,0,1), '+rXP/70+'px '+rYP/12+'px rgba(0,159,227,.7)');
-   });
 
-});
 
 
 
